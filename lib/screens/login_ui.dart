b39daeb3805576cd676e603ded8a6cca41217d56
Thongek Wogponom <70640558+Thongek-wpm@users.noreply.github.com>
2022-12-08 22:37:33 +0700

    commit

--- lib/screens/login_ui.dart
+++ lib/screens/login_ui.dart
@@ -2,7 +2,6 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_demo/model/profile.dart';
 import 'package:flutter_application_demo/screens/home_ui.dart';

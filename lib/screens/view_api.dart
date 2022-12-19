5646c183c5cc209c7fe093fcdf9bdb4a9afd9501
Thongek Wogponom <70640558+Thongek-wpm@users.noreply.github.com>
2022-12-19 17:43:40 +0700

    commit

--- lib/screens/view_api.dart
+++ lib/screens/view_api.dart
@@ -4,8 +4,7 @@ import '../network/feel.dart';
 
 class ViewAPI extends StatelessWidget {
   final Coffee coffee;
-
-  ViewAPI(this.coffee); // ig
+  const ViewAPI(this.coffee, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +16,47 @@ class ViewAPI extends StatelessWidget {
           },
           icon: Icon(Icons.arrow_back_ios),
         ),
-        title: Text(coffee.headtitle),
-        centerTitle: true,
       ),
+      body: Card(
+          margin: EdgeInsets.only(
+            top: 10,
+            bottom: 200,
+            right: 8,
+            left: 8,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Title(
+                  color: Colors.black,
+                  child: Text(
+                    coffee.headtitle,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: ClipRect(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Image.network(
+                      coffee.image,
+                      height: 300,
+                      width: 300,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(coffee.description),
+              )
+            ],
+          )),
     );
   }
 }
